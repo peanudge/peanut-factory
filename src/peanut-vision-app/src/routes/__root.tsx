a2c6dd2e0fa72b800450af39bf4acc178ba1932b
peanut-lover <jihoson94@gmail.com>
2026-06-03 18:25:53 +0900

    feat: add QueryClientProvider and ToastProvider to root

--- src/peanut-vision-app/src/routes/__root.tsx
+++ src/peanut-vision-app/src/routes/__root.tsx
@@ -1,30 +1,31 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Gnb } from '@/components/Gnb'
+import { ToastProvider } from '@/contexts/ToastContext'
+import { ToastContainer } from '@/contexts/ToastContainer'
 
-export const Route = createRootRoute({
-  component: RootComponent,
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
 })
+
+export const Route = createRootRoute({ component: RootComponent })
 
 function RootComponent() {
   return (
-    <>
-      <Gnb />
-      <main>
-        <Outlet />
-      </main>
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'TanStack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <Gnb />
+        <main>
+          <Outlet />
+        </main>
+        <ToastContainer />
+        <TanStackDevtools
+          config={{ position: 'bottom-right' }}
+          plugins={[{ name: 'TanStack Router', render: <TanStackRouterDevtoolsPanel /> }]}
+        />
+      </ToastProvider>
+    </QueryClientProvider>
   )
 }
